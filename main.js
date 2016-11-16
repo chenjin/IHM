3ad40157fe14337c1927653ba618e3a755dce56d
anonymousliu <1532745146@qq.com>
2016-11-17 00:52:09 +0800

    no message

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var app = express()
 var path = require('path');
 var server = require('http').Server(app);
 app.use(express.static(path.join(__dirname, 'js')));
+app.use(express.static(path.join(__dirname, 'css')));
 app.get('/', function(req, res){                  
     res.sendFile(__dirname + '/random_index_jc.html');
 });
