9913234e96883b5c79eaf526cc6f301717062e62
anonymousliu <1532745146@qq.com>
2016-11-17 00:06:13 +0800

    引用本地js文件

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-var app = require('express')();
+var express = require('express')
+var app = express()
+var path = require('path');
 var server = require('http').Server(app);
-
+app.use(express.static(path.join(__dirname, 'js')));
 app.get('/', function(req, res){                  
     res.sendFile(__dirname + '/random_index_jc.html');
 });
